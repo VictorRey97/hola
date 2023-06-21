6fb451836a3b3a18b3b2309ee8e34e9672b55402
VictorRey97 <131829839+VictorRey97@users.noreply.github.com>
2023-06-20 21:51:34 -0600

    Add files via upload

---
+++ carrito78.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+  <title>Procesando Formulario de Envío</title>
+</head>
+<body>
+<table style="width:100%;" align="left">
+		<tr BGCOLOR="#DF2CF1">
+			<th align="left"><h1><FONT COLOR="WHITE" SIZE="7" FACE="Times New Roman"><a href="index.html.php" title="Catalogo">MUEBLERIA ABI</h1></a></FONT></th></p>
+		<th><FONT COLOR="WHITE" SIZE="5" FACE="Times New Roman"><a href="catalogo.html" title="Catalogo">Catalogo</th></font>
+		<th><FONT COLOR="WHITE" SIZE="5" FACE="Times New Roman"><a href="Formulario.html" title="Formulario"> Comentarios</th></a></font>
+		<th><FONT COLOR="WHITE" SIZE="5" FACE="Times New Roman"><a href="inicio-sesion.html" title="Iniciarsesion"> Iniciar sesion</th></a></font>
+
+</table>
+  <?php
+  // Verificar si se ha enviado el formulario
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
+    $nombre = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $ciudad = $_POST["ciudad"];
+    $estado = $_POST["estado"];
+    $codigoPostal = $_POST["codigo_postal"];
+    
+    // Realizar validaciones necesarias
+    
+    // Procesar los datos (guardar en la base de datos, enviar un correo electrónico, etc.)
+    
+    // Mostrar un mensaje de éxito
+    echo "<p>Nombre: $nombre</p>";
+    echo "<p>Dirección: $direccion</p>";
+    echo "<p>Ciudad: $ciudad</p>";
+    echo "<p>Estado: $estado</p>";
+    echo "<p>Código Postal: $codigoPostal</p>";
+  }
+  ?>
+  <table>
+
+  <tr>
+    <td><img src="img/recamara-king-size.jpg" style="width: 700px;"></td>
+    <td><p><h1><FONT COLOR="BLACK" SIZE="6" FACE="Times New Roman">Recamara King Size Chicago Black 4 Pzs</h1></FONT>
+        <br><br>
+        
+        <FONT COLOR="BLACK" SIZE="5" FACE="Times New Roman">
+    
+            Precio normal<br>
+            <s>$30,000.00</s><br>
+            Precio especial<br>
+            $14,000.00
+        <br><br>
+        Incluye: Cabecera king size, dos burós y espejo vertical
+    
+  <br><br>
+	<form action="gracias.html" method="post">
+		<input type="submit"  name="gracias" value="Comprar">
+		</form>
+	<br><br><br><br>
+	</font></td></p>
+</tr>
+  </table>
+  
+
+</body>
+</html>
---
+++ carrito80.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+  <title>Procesando Formulario de Envío</title>
+</head>
+<body>
+<table style="width:100%;" align="left">
+		<tr BGCOLOR="#DF2CF1">
+			<th align="left"><h1><FONT COLOR="WHITE" SIZE="7" FACE="Times New Roman"><a href="index.html.php" title="Catalogo">MUEBLERIA ABI</h1></a></FONT></th></p>
+		<th><FONT COLOR="WHITE" SIZE="5" FACE="Times New Roman"><a href="catalogo.html" title="Catalogo">Catalogo</th></font>
+		<th><FONT COLOR="WHITE" SIZE="5" FACE="Times New Roman"><a href="Formulario.html" title="Formulario"> Comentarios</th></a></font>
+		<th><FONT COLOR="WHITE" SIZE="5" FACE="Times New Roman"><a href="inicio-sesion.html" title="Iniciarsesion"> Iniciar sesion</th></a></font>
+
+</table>
+  <?php
+  // Verificar si se ha enviado el formulario
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
+    $nombre = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $ciudad = $_POST["ciudad"];
+    $estado = $_POST["estado"];
+    $codigoPostal = $_POST["codigo_postal"];
+    
+    // Realizar validaciones necesarias
+    
+    // Procesar los datos (guardar en la base de datos, enviar un correo electrónico, etc.)
+    
+    // Mostrar un mensaje de éxito
+    echo "<p>Nombre: $nombre</p>";
+    echo "<p>Dirección: $direccion</p>";
+    echo "<p>Ciudad: $ciudad</p>";
+    echo "<p>Estado: $estado</p>";
+    echo "<p>Código Postal: $codigoPostal</p>";
+  }
+  ?>
+  <table>
+
+  <tr>
+    <td><img src="img/sillas-atlas.jpg" style="width: 700px;"></td>
+    <td><p><h1><FONT COLOR="BLACK" SIZE="6" FACE="Times New Roman">Sillas Atlas</h1></FONT>
+        <br><br>
+        
+        <FONT COLOR="BLACK" SIZE="5" FACE="Times New Roman">
+    
+        Precio<br>
+        $500.00
+        <br><br>
+        Incluye: 1 Silla atlas
+    
+  <br><br>
+	<form action="gracias.html" method="post">
+		<input type="submit"  name="gracias" value="Comprar">
+		</form>
+	<br><br><br><br>
+	</font></td></p>
+</tr>
+  </table>
+  
+
+</body>
+</html>
